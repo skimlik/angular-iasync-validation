ca27d22568d7b251354c5fbc901121d268c9b4d2
Serhii Kimlyk <serhii.kimlyk@wolterskluwer.com>
2021-03-01 14:21:21 +0200

    fix: code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { FileService } from './file.service';
 import { UniqueFileNameValidator } from './unique.validator';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [AppComponent, HelloComponent, UniqueFileNameValidator],
+  declarations: [AppComponent, UniqueFileNameValidator],
   providers: [FileService],
   bootstrap:    [ AppComponent ]
 })
