a50b1d322d3a6121193d40892a4731573ea3a993
skimlik <kimlik.s@gmail.com>
2021-03-01 12:28:38 +0200

    2 files modified, 1 added

---
+++ src/app/unique.validator.ts
@@ -0,0 +1,24 @@
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  AsyncValidator,
+  NG_ASYNC_VALIDATORS,
+  ValidationErrors
+} from "@angular/forms";
+import { Observable, of } from "rxjs";
+
+@Directive({
+  selector: "[customAsyncValidator]",
+  providers: [
+    {
+      provide: NG_ASYNC_VALIDATORS,
+      useExisting: CustomAsyncValidatorDirective,
+      multi: true
+    }
+  ]
+})
+export class CustomAsyncValidatorDirective implements AsyncValidator {
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    return of({ custom: true });
+  }
+}
